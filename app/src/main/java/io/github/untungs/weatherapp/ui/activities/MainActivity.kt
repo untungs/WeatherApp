77c63253ccfaf4abcadf614796578976587957d9
Untung Suryono <untungs30@gmail.com>
2017-06-30 12:17:56 +0700

    request forecast from API

--- app/src/main/java/io/github/untungs/weatherapp/ui/activities/MainActivity.kt
+++ app/src/main/java/io/github/untungs/weatherapp/ui/activities/MainActivity.kt
@@ -1,9 +1,15 @@
-package io.github.untungs.weatherapp
+package io.github.untungs.weatherapp.ui.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import io.github.untungs.weatherapp.ui.adapters.ForecastListAdapter
+import io.github.untungs.weatherapp.R
+import io.github.untungs.weatherapp.data.Request
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.longToast
+import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
     private val items = listOf(
@@ -22,5 +28,13 @@ class MainActivity : AppCompatActivity() {
         val forecastList = findViewById(R.id.forecast_list) as RecyclerView
         forecastList.layoutManager = LinearLayoutManager(this)
         forecastList.adapter = ForecastListAdapter(items)
+
+        val url = "http://api.openweathermap.org/data/2.5/forecast/daily?" +
+                "APPID=15646a06818f61f7b8d7823ca833e1ce&q=94043&mode=json&units=metric&cnt=7"
+
+        doAsync {
+            Request(url).run()
+            uiThread { longToast("Request performed") }
+        }
     }
 }
--- app/src/main/java/io/github/untungs/weatherapp/ui/adapters/ForecastListAdapter.kt
+++ app/src/main/java/io/github/untungs/weatherapp/ui/adapters/ForecastListAdapter.kt
@@ -1,4 +1,4 @@
-package io.github.untungs.weatherapp
+package io.github.untungs.weatherapp.ui.adapters
 
 import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
